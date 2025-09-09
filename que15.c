c6de14f253510117989fe04b4167e80a08627afc
Riddhi Dabhi <riddhidabhi28@gmail.com>
2025-09-09 10:45:12 +0530

    Delete que15.c

--- que15.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int a,b;
-printf("Enter the temperature in fahrenheit:");
-scanf("%d",&a);
-b= (a-32)*5/9;
-printf("(%d-32)*5/9=%d is the temperature in celcius.", a,b);
-}
